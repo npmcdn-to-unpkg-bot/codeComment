f8cc24441b4bb36160e52bd76f66a876ec29e51b
Lake Black <Lake@Roberts-MacBook-Air.local>
2016-08-25 16:04:01 -0400

    add files twice

---
+++ dist/data.js
@@ -0,0 +1,73 @@
+var data = [
+  {
+    title: 'Nature',
+    user: 'Mark',
+    likes: 7,
+    images: [
+      'assets/images/Nature/p1.jpg',
+      'assets/images/Nature/p2.jpg',
+      'assets/images/Nature/p3.jpg',
+      'assets/images/Nature/p4.jpg',
+      'assets/images/Nature/p5.jpg',
+      'assets/images/Nature/p6.jpg',
+      'assets/images/Nature/p7.jpg',
+      'assets/images/Nature/p8.jpg',
+      'assets/images/Nature/p9.jpg',
+      'assets/images/Nature/p10.jpg'
+    ]
+  },
+  {
+    title: 'Cities',
+    user: 'Mark',
+    likes: 19,
+    images: [
+      'assets/images/Cities/p1.jpg',
+      'assets/images/Cities/p2.jpg',
+      'assets/images/Cities/p3.jpg',
+      'assets/images/Cities/p4.jpg',
+      'assets/images/Cities/p5.jpg',
+      'assets/images/Cities/p6.jpg',
+      'assets/images/Cities/p7.jpg'
+    ]
+  },
+  {
+    title: 'UI',
+    user: 'Mark',
+    likes: 1,
+    images: [
+      'assets/images/UI/p1.jpg',
+      'assets/images/UI/p2.jpg',
+      'assets/images/UI/p3.png',
+      'assets/images/UI/p4.jpg',
+      'assets/images/UI/p5.jpg',
+      'assets/images/UI/p6.png'
+    ]
+  },
+  {
+    title: 'Castles',
+    user: 'Mark',
+    likes: 21,
+    images: [
+      'assets/images/Castles/p1.jpg',
+      'assets/images/Castles/p2.jpg',
+      'assets/images/Castles/p3.jpg',
+      'assets/images/Castles/p4.jpg',
+      'assets/images/Castles/p5.jpg',
+      'assets/images/Castles/p6.jpg',
+      'assets/images/Castles/p7.jpg'
+    ]
+  },
+  {
+    title: 'Cars',
+    user: 'Mark',
+    likes: 4,
+    images: [
+      'assets/images/Cars/p1.jpg',
+      'assets/images/Cars/p2.jpg',
+      'assets/images/Cars/p3.jpg',
+      'assets/images/Cars/p4.jpg',
+      'assets/images/Cars/p5.jpg',
+      'assets/images/Cars/p6.jpg'
+    ]
+  },
+];
